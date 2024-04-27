8b9786e0650b09ba9567a5bc9810143cafc4c6b0
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 22:52:02 +0300

    Create 200B-Drinks.java

---
+++ 200B-Drinks.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class Layano {
+
+   
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x =in.nextInt();float sum=0;
+        int arr[]= new int[x];
+        for (int i = 0; i < x; i++) {
+            arr[i]= (in.nextInt());
+            
+        }
+        for (int i = 0; i < x; i++) {
+           sum+=arr[i];
+        }
+        sum/=100;
+        sum/=x;
+        System.out.println(sum*100);
+        
+    }
+}
