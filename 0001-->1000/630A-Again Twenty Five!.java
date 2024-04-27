a61d504e63debbf84234ac2e79bd0ffbcd0120cb
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 00:28:32 +0300

    630A-Again Twenty Five!

---
+++ 0001-->1000/630A-Again Twenty Five!.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public  class Layanooo{
+
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+        String   n= in.next();
+        System.out.println(25);
+   
+     
+    }
+}
