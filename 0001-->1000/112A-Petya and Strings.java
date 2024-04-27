c00f2d411cb8ff3ebe5bb11f5e52f51eb028b09d
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 23:56:36 +0300

    112A-Petya and Strings

---
+++ 0001-->1000/112A-Petya and Strings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Layanooooo {
+
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String ss = in.next();
+         if (s.compareToIgnoreCase(ss)>0) {
+             System.out.println(1);
+         }
+         else if (s.compareToIgnoreCase(ss)<0) {
+             System.out.println(-1);
+         }
+         else
+             System.out.println(0);
+        
+        
+        
+     
+}}
