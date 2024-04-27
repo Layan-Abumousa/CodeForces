480e02780ba858cfb461e604cef69d10de1a1018
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 23:20:43 +0300

    61A-Ultra-Fast Mathematician

---
+++ 0001-->1000/061A-Ultra-Fast Mathematician.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Layano {
+  
+public static String decimalToBinary(int decimalNumber) {
+        return Integer.toBinaryString(decimalNumber);
+    }
+   
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String ss=in.next();
+        String a="";
+        for (int i = 0; i < s.length(); i++) {
+            char ch=s.charAt(i);
+            char chh=ss.charAt(i);
+            if (ch==chh) {
+                a+='0';
+                
+            }
+            else
+                a+='1';
+        }
+        
+        System.out.println(a);
+      
+        
+        
+    }
+}
