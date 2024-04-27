5fa6a694c326d196f294f0b04eee6cb545f10827
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 23:04:20 +0300

    Create 486A-Calculating Function.cpp

---
+++ 486A-Calculating Function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin>>n;
+    long long int sum=0;
+    if(n%2==0)
+    {
+        sum=n/2;
+    }
+    else
+    {
+        sum=-(n+1)/2;
+    }
+    cout<<sum;
+}
