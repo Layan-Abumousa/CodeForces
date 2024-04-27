5d16ff59782a6a6afc1b230a89df6e09dbbb9a9d
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 23:52:49 +0300

    050A-Domino piling

---
+++ 0001-->1000/050A-Domino piling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Layanooooo {
+
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+         int x = in.nextInt();
+         int y= in.nextInt();
+         System.out.println((x*y)/2);
+         
+        }}
