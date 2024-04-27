997c7aa6b06e540e8bb0e4a4235e9c21db73c5c3
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 23:59:37 +0300

    004A-Watermelon

---
+++ 0001-->1000/004A-Watermelon.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Layanooooo {
+
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+      int x =in.nextInt();
+         if (x%2==0  && x!=2) {
+               
+             System.out.println("YES");
+         }
+         else
+             System.out.println("NO");
+         
+         
+         
+         
+         
+     
+}}
