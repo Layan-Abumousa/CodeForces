0c069786a5a627879b50f4d8fff4af54779869aa
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-27 22:55:51 +0300

    Create 705A-Hulk.java

---
+++ 705A-Hulk.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class Layano {
+  
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+        int x =in.nextInt();
+        for (int i = 1; i <=x; i++) {
+            if (i==x) {
+                if (i%2==0) {
+                System.out.print("I love it"+" ");
+            }
+            else
+                System.out.print("I hate it"+" ");
+            }
+            else{
+            if (i%2==0) {
+                System.out.print("I love that"+" ");
+            }
+            else
+                System.out.print("I hate that"+" ");
+        }}
+      
+      
+    }
+}
