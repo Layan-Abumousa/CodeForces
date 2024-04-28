d932e757568426454c068145bd59d1b848069bf4
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 13:28:38 +0300

    1A-Theatre Square

---
+++ 0001-->1000/001A-Theatre Square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main() {
+ long long n,m,a;
+cin>>n>>m>>a;
+ long long v=ceil((double)m/a)*ceil((double)n/a);
+cout<<v ;
+}
